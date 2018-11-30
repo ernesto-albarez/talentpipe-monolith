8664ab096ed2558780ccefd69a5698760006e667
Sebastian Emanuel Bogado <sebastian.bogado@kimos.io>
2018-11-30 10:13:04 -0300

    Add entities

--- src/main/java/io/kimos/talentppe/MonolithApp.java
+++ src/main/java/io/kimos/talentppe/MonolithApp.java
@@ -10,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
@@ -21,7 +20,7 @@ import java.util.Collection;
 
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MonolithApp {
 
     private static final Logger log = LoggerFactory.getLogger(MonolithApp.class);
